343221a750201cba13ce6c520b0a13162138fbb2
Nielk1 <development@nielk1.com>
2015-05-16 20:17:44 -0500

    Updated IA lua with more test code, having issues
    Vector doesn't want to work.  It's unclear if it's an issue with the use
    of an ffi struct or a scope issue.

--- lua/GameMode/InstantAction.lua
+++ lua/GameMode/InstantAction.lua
@@ -8,8 +8,21 @@ _G.MissionData.Text = "STRING";
 
 hook.Add( "Update", "Chilli_Custom_Update", function( GameTurn )
   if GameTurn % 10 == 0 then
-    _G.PrintConsoleMessage("In Update Turn: " .. GameTurn);
+    --_G.PrintConsoleMessage("In Update Turn: " .. GameTurn);
+    if _G.MissionData.TargetTestIsObjectified then
+      _G.MissionData.TargetTest:SetObjectiveOff();
+    else
+      _G.MissionData.TargetTest:SetObjectiveOn();
+    end
+    _G.MissionData.TargetTestIsObjectified = not _G.MissionData.TargetTestIsObjectified;
   end
+end )
+
+hook.Add( "InitialSetup", "Chilli_Custom_InitialSetup", function()
+  local player = _G.GetPlayerHandle(0);
+  local vectorLoc = _G.GetPositionNear(player:GetPositionV(), 10, 10);
+  _G.MissionData.TargetTest = _G.BuildObject("ibcrate", 0, vectorLoc);
+  _G.MissionData.TargetTestIsObjectified = false;
 end )
 
 _G.PrintConsoleMessage("Loadinged GameMode.InstantAction");
