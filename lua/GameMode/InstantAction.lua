38d2302c1e2f9167b193810ef73c4ad473e0d5f0
Nielk1 <development@nielk1.com>
2015-05-16 23:24:25 -0500

    Testing for GameObject saving in place
    note save code is not yet ready for use, this script is a test case for
    complex data type saving/loading.

--- lua/GameMode/InstantAction.lua
+++ lua/GameMode/InstantAction.lua
@@ -7,6 +7,11 @@ module( "InstantAction" )
 _G.MissionData.Text = "STRING";
 
 hook.Add( "Update", "Chilli_Custom_Update", function( GameTurn )
+  if ( _G.MissionData.TargetTest == nil
+     or not _G.MissionData.TargetTest:IsAround() ) then
+    SpawnBox();
+  end
+
   if GameTurn % 10 == 0 then
     --_G.PrintConsoleMessage("In Update Turn: " .. GameTurn);
     if _G.MissionData.TargetTestIsObjectified then
@@ -18,13 +23,18 @@ hook.Add( "Update", "Chilli_Custom_Update", function( GameTurn )
   end
 end )
 
-hook.Add( "InitialSetup", "Chilli_Custom_InitialSetup", function()
-  local player = _G.GetPlayerHandle(0);
+function SpawnBox()
+  local player = _G.GetPlayerHandle(1);
   local PlayerLocation = player:GetPositionV();
-  _G.PrintConsoleMessage(PlayerLocation.x .. ", " .. PlayerLocation.y .. ", " .. PlayerLocation.z);
-  local vectorLoc = _G.GetPositionNear(PlayerLocation, 1, 1);
-  _G.MissionData.TargetTest = _G.BuildObject("ibcrat00", 0, vectorLoc);
+  --_G.PrintConsoleMessage(PlayerLocation.x .. ", " .. PlayerLocation.y .. ", " .. PlayerLocation.z);
+  local vectorLoc = _G.SetCircularPos(PlayerLocation, _G.GetRandomFloat(10)+20, _G.GetRandomFloat(6.28318530718));
+  --_G.PrintConsoleMessage(vectorLoc.x .. ", " .. vectorLoc.y .. ", " .. vectorLoc.z);
+  _G.MissionData.TargetTest = _G.BuildObject("ibcrat00", 2, vectorLoc);
   _G.MissionData.TargetTestIsObjectified = false;
+end
+
+hook.Add( "InitialSetup", "Chilli_Custom_InitialSetup", function()
+  SpawnBox();
 end )
 
 _G.PrintConsoleMessage("Loadinged GameMode.InstantAction");
